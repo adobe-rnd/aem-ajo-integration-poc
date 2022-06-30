7cc96ef5235f1d4ab3818eb409dab3301f73bf7f
edoardo-goracci <egoracci@rockware.de>
2022-06-30 14:38:58 +0200

    Changed clientLibRes fetching method

--- bundles/core/src/main/java/com/adobe/cq/email/core/components/servlets/AllowedClientLibs.java
+++ bundles/core/src/main/java/com/adobe/cq/email/core/components/servlets/AllowedClientLibs.java
@@ -73,7 +73,7 @@ public class AllowedClientLibs extends SlingSafeMethodsServlet {
         if (policyMgr != null) {
             ContentPolicy policy = policyMgr.getPolicy(contentResource);
             if (policy != null) {
-                Resource clientLibsRes = resolver.getResource(policy.getPath() + "/" + PN_ALLOWED_CLIENT_LIBS);
+                Resource clientLibsRes = resolver.getResource(policy.adaptTo(Resource.class), PN_ALLOWED_CLIENT_LIBS);
                 if (clientLibsRes == null) {
                     return Collections.emptyList();
                 }
